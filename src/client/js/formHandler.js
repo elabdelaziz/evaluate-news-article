df708f963083cc28dec3c7144a22a02238ba7dfc
Abdulaziz Ahmed <abdelaziz.ahmd74@gmail.com>
2021-09-23 18:24:31 +0200

    feat: added API fetching + testing

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,64 @@
+import "regenerator-runtime/runtime.js";
+
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  // check what text was put into the form field
+  let formText = document.getElementById("name").value;
+  //   Client.checkForName(formText);
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+  sendUrlToServer("http://localhost:8081/add", { url: formText }).then(
+    (finalData) => {
+      console.log(finalData);
+      // document.getElementById("text").innerHTML = `text: ${finalData[0].text}`;
+
+      document.getElementById(
+        "agreement"
+      ).innerHTML = `Agreement: ${finalData.agreement}`;
+
+      document.getElementById(
+        "confidence"
+      ).innerHTML = `confidence: ${finalData.confidence}`;
+
+      document.getElementById(
+        "subjectivity"
+      ).innerHTML = `subjectivity: ${finalData.subjectivity}`;
+
+      document.getElementById("irony").innerHTML = `irony: ${finalData.irony}`;
+
+      document.getElementById(
+        "score-tag"
+      ).innerHTML = `score-tag: ${finalData.score_tag}`;
+    }
+  );
+
+  // console.log("::: Form Submitted :::");
+  // fetch("http://localhost:8081/test")
+  //   .then((res) => res.json())
+  //   .then(function (res) {
+  //     document.getElementById("results").innerHTML = res.message;
+  //   });
 }
 
-export { handleSubmit }
+const sendUrlToServer = async (url = "", data = {}) => {
+  // console.log("testing data", data);
+  // console.log("link", url);
+  const serverResponse = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const serverResponseData = await serverResponse.json();
+    // console.log("data received", serverResponseData);
+    return serverResponseData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,46 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+require("dotenv").config();
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fetch = require("node-fetch");
 
-const app = express()
+const API_URL = process.env.API_ID;
+const API_KEY = process.env.API_KEY;
 
-app.use(express.static('dist'))
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("dist"));
 
-console.log(__dirname)
+// console.log(__dirname);
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html"); // check again
+  // res.sendFile(path.resolve("src/client/views/index.html"));
+});
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8081, function () {
+  console.log("Example app listening on port 8081!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
+
+app.post("/add", async (req, res) => {
+  const blogAddress = req.body.url;
+  sendUrlData = `${API_URL}?key=${API_KEY}&url=${blogAddress}&lang=en`;
+
+  const apiResponse = await fetch(sendUrlData);
+  const articleData = await apiResponse.json();
+  console.log(res);
+  res.send(articleData);
+  console.log("sent");
+});
+
+module.exports = {
+  app,
+};
